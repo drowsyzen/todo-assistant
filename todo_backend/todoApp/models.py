fc0bb1484332777c7e64a5778c40f1cc21addd29
drowsy <away28618@gmail.com>
2024-07-15 15:24:14 +0530

    added URL and todo Model

--- todo_backend/todoApp/models.py
+++ todo_backend/todoApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+class BaseModel(models.Model):
+
+    STATUS_CHOICES = (
+        ("Active", 'active'),
+        ("In-Active", 'inactive'),
+        )
+    
+    id           = models.AutoField(primary_key=True,db_index=True)
+    status       = models.CharField(choices=STATUS_CHOICES,default="Active",max_length=100)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
 # Create your models here.
+class TodoModel(BaseModel):
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    
